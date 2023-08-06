e35fdeaedba2876c2815bd7327d166e98e270526
RileyManda <rileymanda69@gmail.com>
2023-08-06 19:56:41 +0200

    Display data from api:location nz

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // async thunk for fetching data
 export const fetchData = createAsyncThunk('home/fetchData', async () => {
   try {
-    const response = await axios.get('https://');
+    const response = await axios.get('https://api.openaq.org/v2/latest?limit=10&page=1&offset=0&sort=desc&radius=1000&country_id=nz&order_by=lastUpdated&dumpRaw=false');
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch data.');
@@ -25,21 +25,21 @@ const homeSlice = createSlice({
       const displayData = 'Home states';
       state.home = displayData;
     },
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchData.pending, (state) => {
-          state.isLoading = true;
-          state.error = undefined;
-        })
-        .addCase(fetchData.fulfilled, (state, action) => {
-          state.isLoading = false;
-          state.home = action.payload.map((home) => ({ ...home }));
-        })
-        .addCase(fetchData.rejected, (state, action) => {
-          state.isLoading = false;
-          state.error = action.error.message;
-        });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.isLoading = true;
+        state.error = undefined;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.home = action.payload.results;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
