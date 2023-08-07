2545fd906d8a1dd579efae961bac2154c32a7f6d
RileyManda <rileymanda69@gmail.com>
2023-08-07 23:06:31 +0200

    Detail view list ui layout

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import bgImage from '../assets/images/world.svg';
 
 const Details = () => {
   const { location } = useParams();
@@ -21,10 +23,16 @@ const Details = () => {
 
   return (
     <div>
-      <h2>{locationData.location}</h2>
-      <div>
-        {locationData.measurements.map((metric) => (
-          <div key={metric.parameter}>
+      <Card className="top-card">
+        <div className="image-container" style={{ backgroundImage: `url(${bgImage})` }} />
+        <div className="image-content">
+          <Card.Title className="top-card-title white-text"><h2>{locationData.location}</h2></Card.Title>
+        </div>
+        <Card.Footer className="detail-text">City/Town BREAKDOWN-2023</Card.Footer>
+      </Card>
+      <div className="detail-list">
+        {locationData.measurements.map((metric, index) => (
+          <div key={metric.parameter} className={`card-detail ${index % 2 === 1 ? 'darker' : ''}`}>
             {metric.parameter}
             :
             {' '}
