470df5cbdde8f98956b62c81cb0519f52508325f
RileyManda <rileymanda69@gmail.com>
2023-08-08 18:02:08 +0200

    Implemented top card to detailview

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import TopCard from './TopCard';
 import Map1 from '../assets/images/map1.svg';
 
 const Details = () => {
@@ -25,14 +25,11 @@ const Details = () => {
 
   return (
     <div>
-      <Card className="top-card">
-        <div className="image-container" style={{ backgroundImage: `url(${Map1})` }} />
-        <div className="top-title-container">
-          <Card.Title className="top-title white-text"><h2>{locationData.location}</h2></Card.Title>
-          <Card.Title className="sub-text white-text"><h2>{locationData.country}</h2></Card.Title>
-        </div>
-        <Card.Footer className="footer-text white-text">City/Town BREAKDOWN-2023</Card.Footer>
-      </Card>
+      <TopCard
+        backgroundImage={Map1}
+        location={locationData.location}
+        views={locationData.country}
+      />
       <div className="detail-list-container">
         {locationData.measurements.map((metric, index) => (
           <div key={metric.parameter} className={`card-detail ${index % 2 === 1 ? 'darker' : ''}`}>
