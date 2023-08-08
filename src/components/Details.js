60a1f4a4d2da543bfce3a1f11819a87eac2325ef
RileyManda <rileymanda69@gmail.com>
2023-08-08 06:42:27 +0200

    Updated maps and alignment

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import bgImage from '../assets/images/world.svg';
+import Map1 from '../assets/images/map1.png';
 
 const Details = () => {
   const { location } = useParams();
@@ -24,11 +24,11 @@ const Details = () => {
   return (
     <div>
       <Card className="top-card">
-        <div className="image-container" style={{ backgroundImage: `url(${bgImage})` }} />
-        <div className="image-content">
-          <Card.Title className="top-card-title white-text"><h2>{locationData.location}</h2></Card.Title>
+        <div className="image-container" style={{ backgroundImage: `url(${Map1})` }} />
+        <div className="detail-content">
+          <Card.Title className="detail-top-title white-text"><h2>{locationData.location}</h2></Card.Title>
         </div>
-        <Card.Footer className="detail-text">City/Town BREAKDOWN-2023</Card.Footer>
+        <Card.Footer className="detail-text white-text">City/Town BREAKDOWN-2023</Card.Footer>
       </Card>
       <div className="detail-list">
         {locationData.measurements.map((metric, index) => (
