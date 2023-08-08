7152cf3b047e3b6b5f4f86c6f9cd0fbd85b9c49d
RileyManda <rileymanda69@gmail.com>
2023-08-08 13:47:39 +0200

    Updated header brand

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-// import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faMicrophone, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faMicrophone, faCog } from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => (
-  <div>
+const Header = () => {
+  const location = useLocation();
+
+  return (
     <Navbar>
-      <Container className="white-text">
-        <Navbar.Brand className="justify-content-start white-text">
-          <Navbar.Brand>
-            <FontAwesomeIcon icon={faChevronLeft} style={{ marginRight: '5px', color: '#fff' }} />
-          </Navbar.Brand>
-          <Navbar.Text style={{ marginRight: '5px', color: '#fff' }}>
-            2015
-          </Navbar.Text>
-        </Navbar.Brand>
-        <Navbar.Brand className="justify-content-start white-text">
-          <Navbar.Text style={{ marginRight: '5px', color: '#fff' }}>
-            most views
-          </Navbar.Text>
+      <Container>
+        <NavLink to="/" className={location.pathname === '/' ? 'active-link' : ''} style={{ textDecoration: 'none', color: '#fff' }}>
+          <FontAwesomeIcon icon={faChevronLeft} style={{ marginRight: '5px' }} />
+          2015
+        </NavLink>
+        <Navbar.Brand className="white-text">
+          most views
         </Navbar.Brand>
         <Navbar.Brand className="justify-content-end" style={{ marginRight: '12px', color: '#fff' }}>
           <Navbar.Text>
             <FontAwesomeIcon icon={faMicrophone} style={{ marginRight: '16px', color: '#fff' }} />
           </Navbar.Text>
           <Navbar.Text>
-            <FontAwesomeIcon icon={faGear} style={{ marginLeft: '15px', color: '#fff' }} />
+            <FontAwesomeIcon icon={faCog} style={{ marginLeft: '15px', color: '#fff' }} />
           </Navbar.Text>
         </Navbar.Brand>
       </Container>
     </Navbar>
-  </div>
-);
+  );
+};
+
 export default Header;
