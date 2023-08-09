cd4457c4627df826a144f5f8d2dd6766edabd6c8
RileyManda <rileymanda69@gmail.com>
2023-08-09 21:10:43 +0200

    Implemented spinner test to test loading states

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,7 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 const LoadingSpinner = () => (
-  <div className="spinner-container flex-container">
+  <div className="spinner-container flex-container" data-testid="loading-spinner">
     <Spinner animation="border" variant="primary" role="status">
       <span className="visually-hidden white-text">Loading...</span>
     </Spinner>
--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,97 +1,34 @@
 import React from 'react';
-import axios from 'axios';
 import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { MemoryRouter } from 'react-router-dom';
 import Home from '../components/Home';
 
-const mockStore = configureStore([]);
+const mockStore = configureStore([thunk]);
 
 describe('Home Component', () => {
   it('should render loading spinner when isLoading is true', async () => {
-    const mockState = {
+    const store = mockStore({
       home: {
         home: [],
         isLoading: true,
         error: null,
       },
-    };
-
-    const store = mockStore({
-      home: mockState.home,
     });
 
     render(
       <Provider store={store}>
-        <BrowserRouter>
+        <MemoryRouter>
           <Home />
-        </BrowserRouter>
-      </Provider>,
-    );
-
-    const loadingSpinner = screen.getByRole('status');
-    expect(loadingSpinner).toBeInTheDocument();
-  });
-
-  it('should render error message on error', async () => {
-    const mockState = {
-      home: {
-        home: [],
-        isLoading: false,
-        error: 'Data fetching failed',
-      },
-    };
-
-    const store = mockStore({
-      home: mockState.home,
-    });
-
-    axios.get.mockRejectedValueOnce(new Error('Data fetching failed'));
-
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
-      </Provider>,
-    );
-
-    const errorMessage = await screen.findByText('Error: Data fetching failed');
-    expect(errorMessage).toBeInTheDocument();
-  });
-
-  it('should render cards with mock data', async () => {
-    const mockState = {
-      home: {
-        home: [
-          {
-            location: 'Location 1',
-            measurements: [
-              { parameter: 'pm25', value: 10 },
-            ],
-          },
-        ],
-        isLoading: false,
-        error: null,
-      },
-    };
-
-    const store = mockStore({
-      home: mockState.home,
-    });
-
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
+        </MemoryRouter>
       </Provider>,
     );
 
     await waitFor(() => {
-      const cards = screen.getAllByRole('link');
-      expect(cards).toHaveLength(mockState.home.home.length);
+      const loadingSpinner = screen.getByTestId('loading-spinner');
+      expect(loadingSpinner).toBeInTheDocument();
     });
   });
 });
